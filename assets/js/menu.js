130ed8b3cc8b889a5f6ab70214148e97848e6bb2
Sridevi <84090678+srideviblogs@users.noreply.github.com>
2025-05-29 14:04:45 -0700

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,4 @@
 function toggleMenu() {
   const nav = document.getElementById('site-nav');
-  const body = document.body;
-
-  if (nav.classList.contains('active')) {
-    nav.classList.remove('active');
-    body.style.overflow = '';
-  } else {
-    nav.classList.add('active');
-    body.style.overflow = 'hidden';
-  }
+  nav.classList.toggle('active');
 }
